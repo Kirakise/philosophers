580a4353f14e5bcaed8236fcb5a57509f5846ae6
Reilly Caraway <rcaraway@am-c3.msk.21-school.ru>
2021-06-12 18:06:49 +0300

    norme

--- philo/philo.h
+++ philo/philo.h
@@ -1,6 +1,5 @@
 #ifndef PHILO_H
 # define PHILO_H
-
 # include <stdio.h>
 # include <sys/time.h>
 # include <unistd.h>
@@ -8,38 +7,37 @@
 # include <stdlib.h>
 # include <stdint.h>
 
-struct s_all;
+struct	s_all;
 
 typedef struct s_philo{
-    int pos;
-    int eats;
-    unsigned long long  lifetime;
-    unsigned long long  leat;
-    int lfork;
-    int rfork;
-    int eatcount;
-    struct s_all *parent;
-    pthread_mutex_t act;
-
-}               t_philo;
+	int					pos;
+	int					eats;
+	unsigned long long	lifetime;
+	unsigned long long	leat;
+	int					lfork;
+	int					rfork;
+	int					eatcount;
+	struct s_all		*parent;
+	pthread_mutex_t		act;
+}						t_philo;
 
 typedef struct s_all{
-    int amount;
-    unsigned long long int tdie;
-    unsigned long long int teat;
-    unsigned long long int tsleep;
-    int eat_count;
-    unsigned long long int start;
-    t_philo *philos;
-    pthread_mutex_t *forks;
-    pthread_mutex_t able_write;
-    pthread_mutex_t	dead;
-}               t_all;
+	int						amount;
+	unsigned long long int	tdie;
+	unsigned long long int	teat;
+	unsigned long long int	tsleep;
+	int						eat_count;
+	unsigned long long int	start;
+	t_philo					*philos;
+	pthread_mutex_t			*forks;
+	pthread_mutex_t			able_write;
+	pthread_mutex_t			dead;
+}							t_all;
 
-int ft_atoi(char *s);
-unsigned long long gettime(void);
-void writemsg(t_philo *ph, int type);
-void *routine(void *arg);
-void *monitor(void *arg);
-void *monitoreat(void *arg);
+int					ft_atoi(char *s);
+unsigned long long	gettime(void);
+void				writemsg(t_philo *ph, int type);
+void				*routine(void *arg);
+void				*monitor(void *arg);
+void				*monitoreat(void *arg);
 #endif
--- philo/write.c
+++ philo/write.c
@@ -1,26 +1,26 @@
 #include "philo.h"
 
-char *getmsg(int type)
+char	*getmsg(int type)
 {
-    if (type == 1)
-        return (" is eating\n");
-    else if (type == 2)
-        return (" is sleeping\n");
-    else if (type == 3)
-        return (" is thinking\n");
-    else if (type == 4)
-        return ("must eat count reached\n");
-    else if (type == 5)
-        return (" taken fork");
-    return (" is dead\n");
+	if (type == 1)
+		return (" is eating\n");
+	else if (type == 2)
+		return (" is sleeping\n");
+	else if (type == 3)
+		return (" is thinking\n");
+	else if (type == 4)
+		return ("must eat count reached\n");
+	else if (type == 5)
+		return (" taken fork");
+	return (" is dead\n");
 }
 
-void writemsg(t_philo *ph, int type)
+void	writemsg(t_philo *ph, int type)
 {
-    pthread_mutex_lock(&ph->parent->able_write);
-    printf("%llu\t", gettime() - ph->parent->start);
-    if (type != 4)
-        printf("%d", ph->pos + 1);
-    printf("%s\n", getmsg(type));
-    pthread_mutex_unlock(&ph->parent->able_write);
+	pthread_mutex_lock(&ph->parent->able_write);
+	printf("%llu\t", gettime() - ph->parent->start);
+	if (type != 4)
+		printf("%d", ph->pos + 1);
+	printf("%s\n", getmsg(type));
+	pthread_mutex_unlock(&ph->parent->able_write);
 }
